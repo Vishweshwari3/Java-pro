6eaa3403f0b5b9f9e12cd28a78995345c396ec3b
Vishweshwari3 <vishweshwarikaparthi@gmail.com>
2024-08-12 16:03:18 +0530

    commit

---
+++ src/Customer.java
@@ -0,0 +1,63 @@
+package day5demo;
+
+public class Customer {
+	private int CustomerId;
+	private String name;
+	private String location;
+	private String email;
+	
+	public void setCustomerId(int CustomerId){
+		this.CustomerId=CustomerId;
+		
+		
+	}
+	public int getCustomerId() {
+		
+		return this.CustomerId;
+		
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	Customer()
+	{
+		this.CustomerId=10;
+		this.name="Abc";
+		this.location="Bnglre";
+		this.email="abc@gmail.com";
+	}
+	Customer(int CustomerId,
+	String name,
+	String location,
+	String email)
+	{
+		this.CustomerId=CustomerId;
+		this.name=name;
+		this.location=location;
+		this.email=email;
+	}
+	
+	public String toString()
+	{
+		return name+"\t"+location+"\t";
+	}
+ 
+
+}
